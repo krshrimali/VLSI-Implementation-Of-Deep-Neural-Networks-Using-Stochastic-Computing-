7bc247d37fc0e9e538dfe7051fd6e0e1cabb81a4
kushashwa ravi shrimali <kushashwaravishrimali@gmail.com>
2018-04-08 22:18:32 +0530

    Added LFSR Code + Images using asic

---
+++ lfsr_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns/1ps
+module lfsr_tb();
+reg clk_tb;
+reg rst_tb;
+wire [3:0] out_tb;
+
+initial
+begin
+    clk_tb = 0;
+    rst_tb = 1;
+    #15;
+    
+    rst_tb = 0;
+    #200;
+end
+
+always
+begin
+    #5;
+    clk_tb = ~ clk_tb;
+end
+
+lfsr DUT(out_tb,clk_tb,rst_tb);
+endmodule
+
